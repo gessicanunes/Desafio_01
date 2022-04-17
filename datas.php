af3f4da382480d85eb0d0c3f1646e051102ece39
Geskness <gsscnns@gmail.com>
2022-04-17 19:36:41 -0300

    Arquivo datas.php adicionado

--- datas.php
+++ datas.php
@@ -15,12 +15,403 @@ function calculaDias($dataInicial, $dataFinal) {
 		- A regra acima não e valida para anos divisiveis por 100. Estes anos devem ser divisiveis por 400 para serem anos bissextos. Assim, o ano 1700, 1800, 1900 e 2100 nao sao bissextos, mas 2000 e bissexto.
 		- Não e permitido o uso de classes e funcoes de data da linguagem (DateTime, mktime, strtotime, etc).
 	*/
+	$anoInicial = substr($dataInicial, -10, 4);
 
-
-return(0);
+if(substr($dataInicial, -5, 1) == 0){
+$mesInicial = substr($dataInicial, -4, 1);
+} else {$mesInicial = substr($dataInicial, -5, 2);}
+$diaInicial = substr($dataInicial, -2, 2);
+$anoFinal = substr($dataFinal, -10, 4);  
+if(substr($dataFinal,-5, 1) == 0){
+$mesFinal = substr($dataFinal, -4, 1);
+} else {$mesInicial = substr($dataInicial, -5, 2);}
+if(substr($dataFinal,-2, 1) == 0){
+$diaFinal = substr($dataFinal, -1, 1);
+} else {
+$diaFinal = substr($dataFinal, -2, 2);	
 }
+$dataFinal = $anoFinal."-".$mesFinal."-".$diaFinal;
 
-$teste = "Variaveis estãp recebendo alguma coisa";
+// Contadores
+$fim = 0;
+
+//Contadoras responsáveis por controlar Do While
+$contAnos = 0;
+$contMesses = $mesInicial;
+$contDias = $diaInicial;
+
+$contAnoFinal = $anoInicial;
+$diasEntreDatas = 0;
+$backupDias = 0;
+
+do { 
+
+	if($contAnoFinal%4 != 0){
+
+		if($contMesses == 1){
+			if($contDias <= 30){
+				$contDias = ++$contDias;
+				$diasEntreDatas = ++$diasEntreDatas;
+			}
+			  else if($contDias == 31){
+				if($dataFinal == ($contAnoFinal."-".$contMesses."-".$contDias)){
+					$backupDias = $backupDias + $contDias;	
+				}else{
+					$backupDias = $backupDias + $contDias;
+					$contDias = 0;
+					$contMesses = ++$contMesses;
+				}
+			  }
+		}else if($contMesses == 2){
+			if($contDias <= 27){
+				$contDias = ++$contDias;
+				$diasEntreDatas = ++$diasEntreDatas;
+			}
+			else if($contDias == 28){
+				if($dataFinal == ($contAnoFinal."-".$contMesses."-".$contDias)){
+					$backupDias = $backupDias + $contDias;	
+				}else{
+					$backupDias = $backupDias + $contDias;
+					$contDias = 0;
+					$contMesses = ++$contMesses;
+				}
+			}
+		}else if($contMesses == 3){
+			if($contDias <= 30){
+				$contDias = ++$contDias;
+				$diasEntreDatas = ++$diasEntreDatas;
+			}
+			  else if($contDias == 31){
+				if($dataFinal == ($contAnoFinal."-".$contMesses."-".$contDias)){
+					$backupDias = $backupDias + $contDias;	
+				}else{
+					$backupDias = $backupDias + $contDias;
+					$contDias = 0;
+					$contMesses = ++$contMesses;
+				}
+			  }
+		}else if($contMesses == 4){
+			if($contDias <= 29){
+				$contDias = ++$contDias;
+				$diasEntreDatas = ++$diasEntreDatas;
+			}
+		     else if($contDias = 30){
+				if($dataFinal == ($contAnoFinal."-".$contMesses."-".$contDias)){
+					$backupDias = $backupDias + $contDias;	
+				}else{
+					$backupDias = $backupDias + $contDias;
+					$contDias = 0;
+					$contMesses = ++$contMesses;
+				}
+			}
+		}else if($contMesses == 5){
+			if($contDias <= 30){
+				$contDias = ++$contDias;
+				$diasEntreDatas = ++$diasEntreDatas;
+			}
+			  else if($contDias == 31){
+				if($dataFinal == ($contAnoFinal."-".$contMesses."-".$contDias)){
+					$backupDias = $backupDias + $contDias;	
+				}else{
+					$backupDias = $backupDias + $contDias;
+					$contDias = 0;
+					$contMesses = ++$contMesses;
+				}
+			  }
+		}else if($contMesses == 6){
+			if($contDias <= 29){
+				$contDias = ++$contDias;
+				$diasEntreDatas = ++$diasEntreDatas;
+			}
+			else if($contDias = 30){
+				if($dataFinal == ($contAnoFinal."-".$contMesses."-".$contDias)){
+					$backupDias = $backupDias + $contDias;	
+				}else{
+					$backupDias = $backupDias + $contDias;
+					$contDias = 0;
+					$contMesses = ++$contMesses;
+				}
+			}
+		}else if($contMesses == 7){
+			if($contDias <= 30){
+				$contDias = ++$contDias;
+				$diasEntreDatas = ++$diasEntreDatas;
+			}
+			  else if($contDias == 31){
+				if($dataFinal == ($contAnoFinal."-".$contMesses."-".$contDias)){
+					$backupDias = $backupDias + $contDias;	
+				}else{
+					$backupDias = $backupDias + $contDias;
+					$contDias = 0;
+					$contMesses = ++$contMesses;
+				}
+			  }
+		}else if($contMesses == 8){
+			if($contDias <= 30){
+				$contDias = ++$contDias;
+				$diasEntreDatas = ++$diasEntreDatas;
+			}
+			else if($contDias == 31){
+				if($dataFinal == ($contAnoFinal."-".$contMesses."-".$contDias)){
+					$backupDias = $backupDias + $contDias;	
+				}else{
+					$backupDias = $backupDias + $contDias;
+					$contDias = 0;
+					$contMesses = ++$contMesses;
+				}
+			}
+		}else if($contMesses == 9){
+			if($contDias <= 29){
+				$contDias = ++$contDias;
+				$diasEntreDatas = ++$diasEntreDatas;
+			}
+			else if($contDias = 30){
+				if($dataFinal == ($contAnoFinal."-".$contMesses."-".$contDias)){
+					$backupDias = $backupDias + $contDias;	
+				}else{
+					$backupDias = $backupDias + $contDias;
+					$contDias = 0;
+					$contMesses = ++$contMesses;
+				}	
+			}
+		}else if($contMesses == 10){
+			if($contDias <= 30){
+				$contDias = ++$contDias;
+				$diasEntreDatas = ++$diasEntreDatas;
+			}
+			  else if($contDias == 31){
+				if($dataFinal == ($contAnoFinal."-".$contMesses."-".$contDias)){
+					$backupDias = $backupDias + $contDias;	
+				}else{
+					$backupDias = $backupDias + $contDias;
+					$contDias = 0;
+					$contMesses = ++$contMesses;
+				}
+			  }
+		}else if($contMesses == 11){
+			if($contDias <= 29){
+				$contDias = ++$contDias;
+				$diasEntreDatas = ++$diasEntreDatas;
+			}else if($contDias = 30){
+				if($dataFinal == ($contAnoFinal."-".$contMesses."-".$contDias)){
+					$backupDias = $backupDias + $contDias;	
+				}else{
+					$backupDias = $backupDias + $contDias;
+					$contDias = 0;
+					$contMesses = ++$contMesses;
+				}
+			}
+		}else if($contMesses == 12){
+			if($contDias <= 30){
+				$contDias = ++$contDias;
+				$diasEntreDatas = ++$diasEntreDatas;
+			}
+			  else if($contDias == 31){
+				if($dataFinal == ($contAnoFinal."-".$contMesses."-".$contDias)){
+					$backupDias = $backupDias + $contDias;
+				  }else{
+					$backupDias = $backupDias + $contDias;
+					$contDias = 0;
+					$contMesses = 1;
+					$contAnos = ++$contAnos;
+					$contAnoFinal = ++$contAnoFinal;
+				  }
+			  }
+		}
+	}else if($contAnoFinal%4 == 0){
+
+			if($contMesses == 1){
+				if($contDias <= 30){
+					$contDias = ++$contDias;
+					$diasEntreDatas = ++$diasEntreDatas;
+				}
+				  else if($contDias == 31){
+					if($dataFinal == ($contAnoFinal."-".$contMesses."-".$contDias)){
+						$backupDias = $backupDias + $contDias;	
+					}else{
+						$backupDias = $backupDias + $contDias;
+						$contDias = 0;
+						$contMesses = ++$contMesses;
+					}
+				  }
+			}else if($contMesses == 2){
+				if($contDias <= 27){
+					$contDias = ++$contDias;
+					$diasEntreDatas = ++$diasEntreDatas;
+				} if($contDias == 28){
+
+				 if($contAnoFinal%100 == 0){
+					 if($contAnoFinal%400 == 0){
+							$contDias = ++$contDias;
+							$diasEntreDatas = ++$diasEntreDatas;
+				}}
+				 if($contAnoFinal%100 == 0){
+					if($contAnoFinal%400 != 0){
+					$contDias = ++$contDias + 0;
+					$backupDias = $backupDias + $contDias;
+					$contDias = 0;
+					$contMesses = ++$contMesses;
+				}}else{
+					$contDias = ++$contDias;
+					$diasEntreDatas = ++$diasEntreDatas;	
+				}}
+				else if($contDias == 29){
+					if($dataFinal == ($contAnoFinal."-".$contMesses."-".$contDias)){
+						$backupDias = $backupDias + $contDias;	
+					}else{
+						$backupDias = $backupDias + $contDias;
+						$contDias = 0;
+						$contMesses = ++$contMesses;
+					}
+				}
+			}else if($contMesses == 3){
+				if($contDias <= 30){
+					$contDias = ++$contDias;
+					$diasEntreDatas = ++$diasEntreDatas;
+				}
+				  else if($contDias == 31){
+					if($dataFinal == ($contAnoFinal."-".$contMesses."-".$contDias)){
+						$backupDias = $backupDias + $contDias;	
+					}else{
+						$backupDias = $backupDias + $contDias;
+						$contDias = 0;
+						$contMesses = ++$contMesses;
+					}
+				  }
+			}else if($contMesses == 4){
+				if($contDias <= 29){
+					$contDias = ++$contDias;
+					$diasEntreDatas = ++$diasEntreDatas;
+				}
+				else if($contDias = 30){
+					if($dataFinal == ($contAnoFinal."-".$contMesses."-".$contDias)){
+						$backupDias = $backupDias + $contDias;	
+					}else{
+						$backupDias = $backupDias + $contDias;
+						$contDias = 0;
+						$contMesses = ++$contMesses;
+					}	
+				}
+			}else if($contMesses == 5){
+				if($contDias <= 30){
+					$contDias = ++$contDias;
+					$diasEntreDatas = ++$diasEntreDatas;
+				}
+				  else if($contDias == 31){
+					if($dataFinal == ($contAnoFinal."-".$contMesses."-".$contDias)){
+						$backupDias = $backupDias + $contDias;	
+					}else{
+						$backupDias = $backupDias + $contDias;
+						$contDias = 0;
+						$contMesses = ++$contMesses;
+					}
+				  }
+			}else if($contMesses == 6){
+				if($contDias <= 29){
+					$contDias = ++$contDias;
+					$diasEntreDatas = ++$diasEntreDatas;
+				}
+				else if($contDias = 30){
+					if($dataFinal == ($contAnoFinal."-".$contMesses."-".$contDias)){
+						$backupDias = $backupDias + $contDias;	
+					}else{
+						$backupDias = $backupDias + $contDias;
+						$contDias = 0;
+						$contMesses = ++$contMesses;
+					}
+				}
+			}else if($contMesses == 7){
+				if($contDias <= 30){
+					$contDias = ++$contDias;
+					$diasEntreDatas = ++$diasEntreDatas;
+				}
+				else if($contDias == 31){
+					if($dataFinal == ($contAnoFinal."-".$contMesses."-".$contDias)){
+						$backupDias = $backupDias + $contDias;	
+					}else{
+						$backupDias = $backupDias + $contDias;
+						$contDias = 0;
+						$contMesses = ++$contMesses;
+					}}
+			}else if($contMesses == 8){
+				if($contDias <= 30){
+					$contDias = ++$contDias;
+					$diasEntreDatas = ++$diasEntreDatas;
+				}
+				else if($contDias == 31){
+					if($dataFinal == ($contAnoFinal."-".$contMesses."-".$contDias)){
+						$backupDias = $backupDias + $contDias;	
+					}else{
+						$backupDias = $backupDias + $contDias;
+						$contDias = 0;
+						$contMesses = ++$contMesses;
+					}
+				}
+			}else if($contMesses == 9){
+				if($contDias <= 29){
+					$contDias = ++$contDias;
+					$diasEntreDatas = ++$diasEntreDatas;
+				}
+				else if($contDias = 30){
+					if($dataFinal == ($contAnoFinal."-".$contMesses."-".$contDias)){
+						$backupDias = $backupDias + $contDias;	
+					}else{
+						$backupDias = $backupDias + $contDias;
+						$contDias = 0;
+						$contMesses = ++$contMesses;
+					}
+				}
+			}else if($contMesses == 10){
+				if($contDias <= 30){
+					$contDias = ++$contDias;
+					$diasEntreDatas = ++$diasEntreDatas;
+				}
+				  else if($contDias == 31){
+					if($dataFinal == ($contAnoFinal."-".$contMesses."-".$contDias)){
+						$backupDias = $backupDias + $contDias;	
+					}else{
+						$backupDias = $backupDias + $contDias;
+						$contDias = 0;
+						$contMesses = ++$contMesses;
+					}
+				  }
+			}else if($contMesses == 11){
+				if($contDias <= 29){
+					$contDias = ++$contDias;
+					$diasEntreDatas = ++$diasEntreDatas;
+				}
+				else if($contDias = 30){
+					if($dataFinal == ($contAnoFinal."-".$contMesses."-".$contDias)){
+						$backupDias = $backupDias + $contDias;	
+					}else{
+						$backupDias = $backupDias + $contDias;
+						$contDias = 0;
+						$contMesses = ++$contMesses;
+					}	
+				}
+			}else if($contMesses == 12){
+				if($contDias <= 30){
+					$contDias = ++$contDias;
+					$diasEntreDatas = ++$diasEntreDatas;
+				}
+				else if($contDias == 31){
+					if($dataFinal == ($contAnoFinal."-".$contMesses."-".$contDias)){
+					  $backupDias = $backupDias + $contDias;
+					}else{
+					  $backupDias = $backupDias + $contDias;
+					  $contDias = 0;
+					  $contMesses = 1;
+					  $contAnos = ++$contAnos;
+					  $contAnoFinal = ++$contAnoFinal;
+					}			  
+				  }
+			}
+	}
+
+} while ($dataFinal != ($contAnoFinal."-".$contMesses."-".$contDias)); 
+
+return ($diasEntreDatas);
+}
 
 /***** Teste 01 *****/
 $dataInicial = "2018-01-01";
@@ -29,6 +420,7 @@ $resultadoEsperado = 1;
 $resultado = calculaDias($dataInicial, $dataFinal);
 verificaResultado("01", $resultadoEsperado, $resultado);
 echo "<br>";
+
 
 /***** Teste 02 *****/
 $dataInicial = "2018-01-01";
@@ -67,19 +459,8 @@ $dataInicial = "2018-01-01";
 $dataFinal = "2019-01-01";
 $resultadoEsperado = 365;
 $resultado = calculaDias($dataInicial, $dataFinal);
-
-$cont = 0;
-do {
-	$anoInicial = substr($dataInicial, -10, 4); // seleciona somento o ano da data inicial
-	$anoFinal = substr($dataFinal, -10, 4);  // selecionan somente d ano da data final
-   
-	if($anoInicial != $anoFinal){ $cont = ++$cont; } 
-
-   } while ($anoFinal != $cont); 
-
-echo $cont, $anoFinal, $anoInicial;
-
 verificaResultado("06", $resultadoEsperado, $resultado);
+
 echo "<br>";
 
 /***** Teste 07 *****/
@@ -168,6 +549,7 @@ $dataFinal = "1900-03-01";
 $resultadoEsperado = 28;
 $resultado = calculaDias($dataInicial, $dataFinal);
 verificaResultado("17", $resultadoEsperado, $resultado);
+
 echo "<br>";
 
 /***** Teste 18 *****/
@@ -192,7 +574,6 @@ $dataFinal = "2001-01-01";
 $resultadoEsperado = 731;
 $resultado = calculaDias($dataInicial, $dataFinal);
 verificaResultado("20", $resultadoEsperado, $resultado);
-
 
 function verificaResultado($nTeste, $resultadoEsperado, $resultado) {
 	if(intval($resultadoEsperado) == intval($resultado)) {
